64f97358f98214f3bc8ab18db7a2575e45f9ab99
Janos <pauer.janos@gmail.com>
2017-07-22 13:31:46 +0200

    lots of stuff

--- js/index.js
+++ js/index.js
@@ -3,13 +3,21 @@ window.onload = function(){
   document.getElementById("today").innerHTML = ('0' + d.getDate()).slice(-2)+"/"+('0' + d.getMonth()).slice(-2)+"/"+d.getFullYear();
 
   document.getElementById("time").innerHTML = ('0' + d.getHours()).slice(-2)+":"+('0' + d.getMinutes()).slice(-2);
+
 };
 
 function disp()
 {
   document.getElementById("signup").style.display = "block";
-}
+};
 function nodisp()
 {
   document.getElementById("signup").style.display = "none";
-}
+};
+
+$(document).ready(function () {
+  setTimeout(function () {
+      $('#claim').hide();
+      // $('#claim').fadeOut('fast');
+  }, 5000);
+});
