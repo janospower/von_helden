d74239c83ef746233f6dbd50b1122ed6e0c6a1a1
Janos <pauer.janos@gmail.com>
2017-07-22 02:19:48 +0200

    added live date & Time

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+  var d = new Date();
+  document.getElementById("today").innerHTML = ('0' + d.getDate()).slice(-2)+"/"+('0' + d.getMonth()).slice(-2)+"/"+d.getFullYear();
+
+  document.getElementById("time").innerHTML = ('0' + d.getHours()).slice(-2)+":"+('0' + d.getMinutes()).slice(-2);
+}
