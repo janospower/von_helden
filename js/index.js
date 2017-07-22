1cdace6517f505663f837130910653afe3579743
Janos <pauer.janos@gmail.com>
2017-07-22 02:43:29 +0200

    header

--- js/index.js
+++ js/index.js
@@ -3,4 +3,4 @@ window.onload = function(){
   document.getElementById("today").innerHTML = ('0' + d.getDate()).slice(-2)+"/"+('0' + d.getMonth()).slice(-2)+"/"+d.getFullYear();
 
   document.getElementById("time").innerHTML = ('0' + d.getHours()).slice(-2)+":"+('0' + d.getMinutes()).slice(-2);
-}
+};
